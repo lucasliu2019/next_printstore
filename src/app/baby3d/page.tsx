f32f82d193ede7c6648c386b7797f29696af1234
lucasliu2019 <zhez@me.com>
2025-03-28 16:19:21 -0500

    update

--- src/app/baby3d/page.tsx
+++ src/app/baby3d/page.tsx
@@ -1,5 +1,5 @@
 import Heading from "../components/heading/heading";
-import Slideshow from "../components/slideshow/slideshow";
+// import Slideshow from "../components/slideshow/slideshow";
 import styles from "./page.module.css";
 
 export default function About() {
