b1ed505f7e17015357d51daebefe9f3e844ab26b
lucasliu2019 <zhez@me.com>
2025-04-15 23:26:04 -0500

    contact form update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,37 @@
 import styles from "./page.module.css";
 import ContactForm from "../components/ContactForm/ContactForm";
+import Image from "next/image";
 
 export default function Contact() {
   return (
     <div className={styles.page}>
-        <h2 className={styles.heading}>Contact us</h2>
+      <h2 className={styles.heading}>Contact us</h2>
+      <div className={styles.contactContainer}>
         <ContactForm />
+        <Image
+          src="/env_blue.png"
+          alt="Blue Envolope"
+          width={500}
+          height={500}
+          className={styles.envBlue}
+        />
+
+        <Image
+          src="/env_yellow.png"
+          alt="Yellow Envelope"
+          width={500}
+          height={500}
+          className={styles.envYellow}
+        />
+
+        <Image
+          src="/env_pink.png"
+          alt="Pink Envelope"
+          width={500}
+          height={500}
+          className={styles.envPink}
+        />
+      </div>
     </div>
   );
 }
