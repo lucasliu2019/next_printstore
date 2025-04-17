a1d23e7b837fb9547eb7d5c43d360f9c4af8f44e
lucasliu2019 <zhez@me.com>
2025-04-16 22:24:59 -0500

    update

--- src/app/baby3d/page.tsx
+++ src/app/baby3d/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
 
