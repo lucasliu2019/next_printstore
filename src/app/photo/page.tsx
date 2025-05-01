aff37e33120223ac8fddd054e4370880e3c31482
lucasliu2019 <zhez@me.com>
2025-04-30 22:40:59 -0500

    update

--- src/app/photo/page.tsx
+++ src/app/photo/page.tsx
@@ -15,17 +15,19 @@ export default function PrintPage() {
         </h1>
 
         <section className={styles.section}>
-          <div className={styles.left}>
-            <h2 className={styles.sectionTitle}>Custom Calendars</h2>
-            <p className={styles.text}>
-              <strong>Looking for a personalized calendar?</strong><br />
-              We create custom calendars designed just for you with your photos, logo, branding, or special dates.<br />
-              <br />
-            </p>
-            <p className={styles.textPricing}>
-              <strong>Wall calendar</strong> (Letter, 12 months)<br />
-              <strong>$50 – $100</strong>
-            </p>
+          <div className={`${styles.left} ${styles.one}`} >
+            <div className={styles.textWrapper}>
+              <h2 className={styles.sectionTitle}>Custom Calendars</h2>
+              <p className={styles.text}>
+                <strong>Looking for a personalized calendar?</strong><br />
+                We create custom calendars designed just for you with your photos, logo, branding, or special dates.<br />
+                <br />
+              </p>
+              <p className={styles.textPricing}>
+                <strong>Wall calendar</strong> (Letter, 12 months)<br />
+                <strong>$50 – $100</strong>
+              </p>
+            </div>
           </div>
           <div className={styles.right}>
             <div className={styles.imageWrapper}>
@@ -33,27 +35,29 @@ export default function PrintPage() {
                 src="/photo_calendar.png"
                 alt="Calendar"
                 fill
-                className={styles.image}
+                className={`${styles.image} ${styles.imgOne}`}
               />
             </div>
           </div>
         </section>
 
         <section className={`${styles.section} ${styles.reverse}`}>
-          <div className={styles.left}>
-            <h2 className={styles.sectionTitle}>Passport & ID Photos</h2>
-            <h2 className={styles.sectionTitle}>Fast and Professional</h2>
-            <ul>
-              <li>Passport photos</li>
-              <li>Visa, ID, driver’s license photos</li>
-              <li>Digital photos for online applications</li>
-              <li>Printed copies in standard formats</li>
-              <li>Fast service — ready in minutes!</li>
-            </ul>
-            <p className={styles.textPricing}>
-              <strong>Passport photo (print + digital)</strong> – 20$<br />
-              <strong>Extra printed copies</strong> – 3$
-            </p>
+          <div className={`${styles.left} ${styles.two}`}>
+            <div className={styles.textWrapper}>
+              <h2 className={styles.sectionTitle}>Passport & ID Photos</h2>
+              <h2 className={styles.sectionTitle}>Fast and Professional</h2>
+              <ul>
+                <li>Passport photos</li>
+                <li>Visa, ID, driver’s license photos</li>
+                <li>Digital photos for online applications</li>
+                <li>Printed copies in standard formats</li>
+                <li>Fast service — ready in minutes!</li>
+              </ul>
+              <p className={styles.textPricing}>
+                <strong>Passport photo (print + digital)</strong> – 20$<br />
+                <strong>Extra printed copies</strong> – 3$
+              </p>
+            </div>
           </div>
           <div className={styles.right}>
             <div className={styles.imageWrapper}>
@@ -61,7 +65,7 @@ export default function PrintPage() {
                 src="/photo_photos.png"
                 alt="Photo Printing"
                 fill
-                className={styles.image}
+                className={`${styles.image} ${styles.imgTwo}`}
               />
             </div>
           </div>
@@ -69,20 +73,22 @@ export default function PrintPage() {
         </section>
 
         <section className={styles.section}>
-          <div className={styles.left}>
-            <h2 className={styles.sectionTitle}>Custom Brochure Design & Printing</h2>
-            <ul>
-              <li>Tri-fold, bi-fold, and multi-page brochures</li>
-              <li>Custom design with your text, images, and branding</li>
-              <li>High-resolution printing on premium paper</li>
-              <li>Glossy or matte finishes</li>
-              <li>Fast turnaround and affordable prices</li>
-            </ul>
-            <p className={styles.textPricing}>
-              Basic design using a template – <strong>20 – 50$</strong><br />
-              Custom design (1–2 pages) – <strong>40 – 60$</strong><br />
-              Full custom tri-fold brochure (6 panels) – <strong>70 – 150$</strong>
-            </p>
+          <div className={`${styles.left} ${styles.three}`}>
+            <div className={styles.textWrapper}>
+              <h2 className={styles.sectionTitle}>Custom Brochure Design & Printing</h2>
+              <ul>
+                <li>Tri-fold, bi-fold, and multi-page brochures</li>
+                <li>Custom design with your text, images, and branding</li>
+                <li>High-resolution printing on premium paper</li>
+                <li>Glossy or matte finishes</li>
+                <li>Fast turnaround and affordable prices</li>
+              </ul>
+              <p className={styles.textPricing}>
+                Basic design using a template – <strong>20 – 50$</strong><br />
+                Custom design (1–2 pages) – <strong>40 – 60$</strong><br />
+                Full custom tri-fold brochure (6 panels) – <strong>70 – 150$</strong>
+              </p>
+            </div>
           </div>
 
 
@@ -92,7 +98,7 @@ export default function PrintPage() {
                 src="/photo_brochure.png"
                 alt="Brochure"
                 fill
-                className={styles.image}
+                className={`${styles.image} ${styles.imgThree}`}
               />
             </div>
           </div>
